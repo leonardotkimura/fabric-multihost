51bc94a2e226eebf9d303084a1102f75cdb0d4d6
Ubuntu User <ubuntu@org1.cs3b7cloud.internal>
2022-10-19 19:31:04 +0000

    regenerate channel artifacts

--- artifacts/channel/create-artifacts.sh
+++ artifacts/channel/create-artifacts.sh
@@ -1,9 +1,9 @@
 
-# chmod -R 0755 ./crypto-config
-# # Delete existing artifacts
-# rm -rf ./crypto-config
-# rm genesis.block mychannel.tx
-# rm -rf ../../channel-artifacts/*
+chmod -R 0755 ./crypto-config
+# Delete existing artifacts
+rm -rf ./crypto-config
+rm genesis.block mychannel.tx
+rm -rf ../../channel-artifacts/*
 
 #Generate Crypto artifactes for organizations
 cryptogen generate --config=./crypto-config.yaml --output=./crypto-config/
@@ -11,7 +11,7 @@ cryptogen generate --config=./crypto-config.yaml --output=./crypto-config/
 
 
 # System channel
-SYS_CHANNEL="mychannel"
+SYS_CHANNEL="sys-channel"
 
 # channel name defaults to "mychannel"
 CHANNEL_NAME="mychannel"
@@ -23,7 +23,7 @@ configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outp
 
 
 # Generate channel configuration block
-configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./channel1.tx -channelID $CHANNEL_NAME
+configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./mychannel.tx -channelID $CHANNEL_NAME
 
 echo "#######    Generating anchor peer update for Org1MSP  ##########"
 configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
