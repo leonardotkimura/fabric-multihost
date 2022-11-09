55ce62826d799e92c79f8436027099159a286c6d
root <root@vm1.cs3b7cloud.internal>
2022-11-09 19:12:43 +0000

    new channel artifacts

---
+++ setup1/machines/deleteArtifacts.sh
@@ -0,0 +1,23 @@
+cd vm0
+sudo rm -r crypto-config
+sudo rm -r create-certificate-with-ca/fabric-ca
+cd -
+
+cd vm1
+sudo rm -r crypto-config
+sudo rm -r create-certificate-with-ca/fabric-ca
+cd -
+
+cd vm2
+sudo rm -r crypto-config
+sudo rm -r create-certificate-with-ca/fabric-ca
+cd -
+
+cd vm3
+sudo rm -r crypto-config
+sudo rm -r create-certificate-with-ca/fabric-ca
+cd -
+
+cd vm4
+sudo rm -r crypto-config
+cd -
