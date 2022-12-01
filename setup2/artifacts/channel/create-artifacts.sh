671dcca6e229c526a6f94ec6872d8951defd16fe
Ubuntu@vm1 <leonardotkimura@gmail.com>
2022-12-01 13:14:01 +0000

    changes channel artifacts

--- setup2/artifacts/channel/create-artifacts.sh
+++ setup2/artifacts/channel/create-artifacts.sh
@@ -33,3 +33,24 @@ configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org2M
 
 echo "#######    Generating anchor peer update for Org3MSP  ##########"
 configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org3MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org3MSP
+
+echo "#######    Generating anchor peer update for Org4MSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org4MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org4MSP
+
+echo "#######    Generating anchor peer update for Org4MSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org4MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org4MSP
+
+echo "#######    Generating anchor peer update for Org5MSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org5MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org5MSP
+
+echo "#######    Generating anchor peer update for Org6MSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org6MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org6MSP
+
+echo "#######    Generating anchor peer update for Org7MSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org7MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org7MSP
+
+echo "#######    Generating anchor peer update for Org8MSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org8MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org8MSP
+
+echo "#######    Generating anchor peer update for Org9MSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org9MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org9MSP
